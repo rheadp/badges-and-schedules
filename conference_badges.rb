1bcee630c00f58097d28d0c5e1e77ca7cfb53405
rheadp <rheadpea@msu.edu>
2021-02-14 20:13:52 -0500

    commit tree 9ce7d1e4424ccfa6eb701ddb8ec80da6233464f0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+    badge_array = []
+    name.each do |message|
+        badge_array << "Hello, my name is #{message}."
+    end
+    badge_array
+end
+
+def assign_rooms(name)
+    room_array = []
+    name.each_with_index do |message, index|
+        room_array << "Hello, #{message}! You'll be assigned to room #{index+1}!"
+    end
+    room_array
+end
+
+def printer(attendes)
+    batch_badge_creator(attendes).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendes).each do |room|
+        puts room
+    end
+end 
